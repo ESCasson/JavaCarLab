aa036ac522f9cbcdc259ce609e88a7789be4eb04
Ellie <cassona08@gmail.com>
2020-02-05 14:46:13 +0000

    customer complete

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -15,4 +15,32 @@ public class CustomerTest {
         assertEquals(300, customer.getMoney(), 2d);
     }
 
+    @Test
+    public void doesHaveOwnedCarArray(){
+        assertEquals(0, customer.countCarsOwned());
+    }
+
+    @Test
+    public void canAddCarToOwnedCarArray(){
+        customer.addCar(1);
+        assertEquals(1, customer.countCarsOwned());
+    }
+
+    @Test
+    public void canRemoveMoney(){
+        customer.removeMoney(100);
+        assertEquals(200, customer.getMoney(), 2d);
+    }
+
+    @Test
+    public void canBuyCar(){
+        int car = 1;
+        double value = 50;
+        customer.buyCar(1,50);
+        assertEquals(250, customer.getMoney(), 2d);
+        assertEquals(1, customer.countCarsOwned());
+
+
+    }
+
 }
